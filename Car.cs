911c37a065863e9f1bb77b11bb31d15890fc5467
wickedvig <57472764+wickedvig@users.noreply.github.com>
2021-03-31 18:01:30 +0530

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TrainingExample1
+{
+    class Car
+    {
+
+        public string model;
+        public string color;
+        public int vehicleno;
+        private int engineno;
+
+
+        private int EngineNo { get { return engineno; } set { engineno = value; } }
+
+
+        public void GetFromInput()
+        {
+            Console.Write("Enter the model of car: ");
+            model = Console.ReadLine();
+            Console.Write("Enter the color of car: ");
+            color = Console.ReadLine();
+            Console.Write("Enter the vehicleno of car: ");
+            vehicleno = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the engineno of car: ");
+            engineno = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            GetFromInput();
+        }
+        public void DisplayCar()
+        {
+            Console.WriteLine("Car Model:{0}, Car Color:{1}, VehicleNum:{2}, EngineNum:{3}", model, color, vehicleno, engineno);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TrainingExample1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car[] car = new Car[3];
+            car[0] = new Car();
+            car[1] = new Car();
+            car[2] = new Car();
+
+            car[0].Display();
+            car[1].Display();
+            car[2].Display();
+
+            car[0].DisplayCar();
+            car[1].DisplayCar();
+            car[2].DisplayCar();
+        }
+    }
+}
